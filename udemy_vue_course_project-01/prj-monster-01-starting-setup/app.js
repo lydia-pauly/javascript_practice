0a3c22d168efec19618ffee98d37860084fb117e
Lydia Pauly <pauly.lydia@gmail.com>
2023-11-16 08:51:01 +0000

    Fixed heal bug and added in health bars

--- udemy_vue_course_project-01/prj-monster-01-starting-setup/app.js
+++ udemy_vue_course_project-01/prj-monster-01-starting-setup/app.js
@@ -9,12 +9,16 @@ const APP = Vue.createApp({
         minAttack: 5,
         minHeal : 1,
         maxHeal : 10,
+        specialRecharge: 0
       },
       monster: {
         health: 130,
         maxHealth: 130,
         maxAttack: 20,
         minAttack: 2
+      },
+      game : {
+        currentRound: 0
       }
     }
   },
@@ -26,6 +30,12 @@ const APP = Vue.createApp({
     monsterPercentageBar() {
       return {width:((this.monster.health / this.monster.maxHealth)*100) + '%'};
     },
+    playerAlive() {
+      return this.player.health !== 0;
+    },
+    canUseSpecialAttack() {
+      return this.player.specialRecharge === 0;
+    }
   },
 
   methods: {
@@ -50,10 +60,10 @@ const APP = Vue.createApp({
 
     changeHealth(current, max, change, heal) {
       if (heal) {
-        if ((current + heal) >= max) {
+        if ((current + change) >= max) {
           return max;
         } else {
-          return (current + heal);
+          return (current + change);
         }
       }
       if (!heal) {
@@ -65,27 +75,37 @@ const APP = Vue.createApp({
       };
     },
 
+    specialRecharge(recharge) {
+      if (recharge > 0) {
+        return recharge--;
+      } else {
+        return 0;
+      }
+    },
+
     attackMonster(special) {
       let attack = 0;
-      console.log(`ATTACK ROUND`);
+      console.log(`ROUND ${this.game.currentRound}`);
       console.log(`Player health: ${this.player.health}`);
       console.log(`Monster health: ${this.monster.health}`);
       if (!special) {
         attack = this.getRandomValue(this.player.minAttack, this.player.maxAttack, false);
         console.log(`Player attacked for ${attack} damage!`);
       } else {
-        attack = this.getRandomValue(this.player.minAttack, this.player.maxAttack, false) + 5;
+        attack = this.getRandomValue(this.player.minAttack, this.player.maxAttack, false) + 25;
         console.log(`Player special attacked for ${attack} damage!`);
       }
       this.monster.health = this.changeHealth(this.monster.health, this.monster.maxHealth, attack, false);
       console.log(`Monster health now ${this.monster.health}`);
       console.log(`---`);
       this.checkDeath();
+      this.player.specialRecharge = this.specialRecharge(this.player.specialRecharge);
       this.attackPlayer();
+      this.game.currentRound ++;
     },
 
     attackPlayer() {
-      console.log(`COUNTER ATTACK ROUND`);
+      console.log(`COUNTER ATTACK`);
       console.log(`Player health: ${this.player.health}`);
       console.log(`Monster health: ${this.monster.health}`);
       let attack = this.getRandomValue(this.monster.minAttack, this.monster.maxAttack, true);
@@ -96,10 +116,10 @@ const APP = Vue.createApp({
       this.checkDeath();
     },
     healPlayer() {
-      console.log(`ATTACK ROUND`);
+      console.log(`ROUND ${this.game.currentRound}`);
       console.log(`Player health: ${this.player.health}`);
       console.log(`Monster health: ${this.monster.health}`);
-      let heal = this.getRandomValue(this.player.health, this.player.maxHealth, heal, true);
+      let heal = this.getRandomValue(this.player.minHeal, this.player.maxHeal);
       if ((this.player.health + heal) > this.player.maxHealth) {
         console.log(`Player max healed for ${this.player.maxHealth - this.player.health} points`);
       } else {
@@ -110,6 +130,8 @@ const APP = Vue.createApp({
       console.log(`---`);
       this.attackPlayer();
       this.checkDeath();
+      this.player.specialRecharge = 3;
+      this.game.currentRound ++;
     }
   }
 });
